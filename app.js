686553fbb160fac019184adca7806f0953dda4d8
nat <nat@local.de>
2023-03-05 14:17:46 +0100

    live long and prosper

--- app.js
+++ app.js
@@ -10,47 +10,30 @@ var typewriter = new Typewriter(app, {
 
 typewriter
   .changeDelay(10)
-  .typeString('<a href="https://world.secondlife.com/resident/140daf33-db90-4451-954c-4fbeceb186c8">⠄⠄⠄⢰⣧⣼⣯⠄⣸⣠⣶⣶⣦⣾⠄⠄⠄⠄⡀⠄⢀⣿⣿⠄⠄⠄⢸⡇⠄⠄</a><br />')
-  .typeString('<a href="https://world.secondlife.com/resident/140daf33-db90-4451-954c-4fbeceb186c8">⠄⠄⠄⣾⣿⠿⠿⠶⠿⢿⣿⣿⣿⣿⣦⣤⣄⢀⡅⢠⣾⣛⡉⠄⠄⠄⠸⢀⣿⠄</a><br />')
-  .typeString('<a href="https://world.secondlife.com/resident/140daf33-db90-4451-954c-4fbeceb186c8">⠄⠄⢀⡋⣡⣴⣶⣶⡀⠄⠄⠙⢿⣿⣿⣿⣿⣿⣴⣿⣿⣿⢃⣤⣄⣀⣥⣿⣿⠄</a><br />')
-  .typeString('<a href="https://world.secondlife.com/resident/140daf33-db90-4451-954c-4fbeceb186c8">⠄⠄⢸⣇⠻⣿⣿⣿⣧⣀⢀⣠⡌⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⣿⣿⣿⠄</a><br />')
-  .typeString('<a href="https://world.secondlife.com/resident/140daf33-db90-4451-954c-4fbeceb186c8">⠄⢀⢸⣿⣷⣤⣤⣤⣬⣙⣛⢿⣿⣿⣿⣿⣿⣿⡿⣿⣿⡍⠄⠄⢀⣤⣄⠉⠋⣰</a><br />')
-  .typeString('<a href="https://world.secondlife.com/resident/140daf33-db90-4451-954c-4fbeceb186c8">⠄⣼⣖⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⢇⣿⣿⡷⠶⠶⢿⣿⣿⠇⢀⣤</a><br />')
-  .typeString('<a href="https://world.secondlife.com/resident/140daf33-db90-4451-954c-4fbeceb186c8">⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⣿⣿⡇⣿⣿⣿⣿⣿⣿⣷⣶⣥⣴⣿⡗</a><br />')
-  .typeString('<a href="https://world.secondlife.com/resident/140daf33-db90-4451-954c-4fbeceb186c8">⢀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠄</a><br />')
-  .typeString('<a href="https://world.secondlife.com/resident/140daf33-db90-4451-954c-4fbeceb186c8">⢸⣿⣦⣌⣛⣻⣿⣿⣧⠙⠛⠛⡭⠅⠒⠦⠭⣭⡻⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠄</a><br />')
-  .typeString('<a href="https://world.secondlife.com/resident/140daf33-db90-4451-954c-4fbeceb186c8">⠘⣿⣿⣿⣿⣿⣿⣿⣿⡆⠄⠄⠄⠄⠄⠄⠄⠄⠹⠈⢋⣽⣿⣿⣿⣿⣵⣾⠃⠄</a><br />')
-  .typeString('<a href="https://world.secondlife.com/resident/140daf33-db90-4451-954c-4fbeceb186c8">⠄⠘⣿⣿⣿⣿⣿⣿⣿⣿⠄⣴⣿⣶⣄⠄⣴⣶⠄⢀⣾⣿⣿⣿⣿⣿⣿⠃⠄⠄</a><br />')
-  .typeString('<a href="https://world.secondlife.com/resident/140daf33-db90-4451-954c-4fbeceb186c8">⠄⠄⠈⠻⣿⣿⣿⣿⣿⣿⡄⢻⣿⣿⣿⠄⣿⣿⡀⣾⣿⣿⣿⣿⣛⠛⠁⠄⠄⠄</a><br />')
-  .typeString('<a href="https://world.secondlife.com/resident/140daf33-db90-4451-954c-4fbeceb186c8">⠄⠄⠄⠄⠈⠛⢿⣿⣿⣿⠁⠞⢿⣿⣿⡄⢿⣿⡇⣸⣿⣿⠿⠛⠁⠄⠄⠄⠄⠄</a><br />')
-  .typeString('<a href="https://world.secondlife.com/resident/140daf33-db90-4451-954c-4fbeceb186c8">⠄⠄⠄⠄⠄⠄⠄⠉⠻⣿⣿⣾⣦⡙⠻⣷⣾⣿⠃⠿⠋⠁⠄⠄⠄⠄⠄⢀⣠⣴</a><br />')
-  .typeString('<a href="https://world.secondlife.com/resident/140daf33-db90-4451-954c-4fbeceb186c8">⣿⣿⣿⣶⣶⣮⣥⣒⠲⢮⣝⡿⣿⣿⡆⣿⡿⠃⠄⠄⠄⠄⠄⠄⠄⣠⣴⣿⣿⣿</a><br />')
   .typeString('<br /><br />')
-  .changeDelay(50)
-  .typeString('Hello there, I am an experienced LSL scripter and not available for hire at this moment.')
-  .pauseFor(2000)
-  .changeDeleteSpeed(1)
-  .deleteChars(39)
-  .typeString(' totally available for hire ;)')
-  .typeString(' <br /> <br />I have worked on a variety of inworld projects over the years. While coding is my primary focus, I have a deep understanding of game asset creation in general. <br /> <br />')
-  .pauseFor(300)
+  .typeString('<a href="ascii.html" target="_blank">⠄⠄⠄⢰⣧⣼⣯⠄⣸⣠⣶⣶⣦⣾⠄⠄⠄⠄⡀⠄⢀⣿⣿⠄⠄⠄⢸⡇⠄⠄</a><br />')
+  .typeString('<a href="ascii.html" target="_blank">⠄⠄⠄⣾⣿⠿⠿⠶⠿⢿⣿⣿⣿⣿⣦⣤⣄⢀⡅⢠⣾⣛⡉⠄⠄⠄⠸⢀⣿⠄</a><br />')
+  .typeString('<a href="ascii.html" target="_blank">⠄⠄⢀⡋⣡⣴⣶⣶⡀⠄⠄⠙⢿⣿⣿⣿⣿⣿⣴⣿⣿⣿⢃⣤⣄⣀⣥⣿⣿⠄</a><br />')
+  .typeString('<a href="ascii.html" target="_blank">⠄⠄⢸⣇⠻⣿⣿⣿⣧⣀⢀⣠⡌⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⣿⣿⣿⠄</a><br />')
+  .typeString('<a href="ascii.html" target="_blank">⠄⢀⢸⣿⣷⣤⣤⣤⣬⣙⣛⢿⣿⣿⣿⣿⣿⣿⡿⣿⣿⡍⠄⠄⢀⣤⣄⠉⠋⣰</a><br />')
+  .typeString('<a href="ascii.html" target="_blank">⠄⣼⣖⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⢇⣿⣿⡷⠶⠶⢿⣿⣿⠇⢀⣤</a><br />')
+  .typeString('<a href="ascii.html" target="_blank">⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⣿⣿⡇⣿⣿⣿⣿⣿⣿⣷⣶⣥⣴⣿⡗</a><br />')
+  .typeString('<a href="ascii.html" target="_blank">⢀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠄</a><br />')
+  .typeString('<a href="ascii.html" target="_blank">⢸⣿⣦⣌⣛⣻⣿⣿⣧⠙⠛⠛⡭⠅⠒⠦⠭⣭⡻⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠄</a><br />')
+  .typeString('<a href="ascii.html" target="_blank">⠘⣿⣿⣿⣿⣿⣿⣿⣿⡆⠄⠄⠄⠄⠄⠄⠄⠄⠹⠈⢋⣽⣿⣿⣿⣿⣵⣾⠃⠄</a><br />')
+  .typeString('<a href="ascii.html" target="_blank">⠄⠘⣿⣿⣿⣿⣿⣿⣿⣿⠄⣴⣿⣶⣄⠄⣴⣶⠄⢀⣾⣿⣿⣿⣿⣿⣿⠃⠄⠄</a><br />')
+  .typeString('<a href="ascii.html" target="_blank">⠄⠄⠈⠻⣿⣿⣿⣿⣿⣿⡄⢻⣿⣿⣿⠄⣿⣿⡀⣾⣿⣿⣿⣿⣛⠛⠁⠄⠄⠄</a><br />')
+  .typeString('<a href="ascii.html" target="_blank">⠄⠄⠄⠄⠈⠛⢿⣿⣿⣿⠁⠞⢿⣿⣿⡄⢿⣿⡇⣸⣿⣿⠿⠛⠁⠄⠄⠄⠄⠄</a><br />')
+  .typeString('<a href="ascii.html" target="_blank">⠄⠄⠄⠄⠄⠄⠄⠉⠻⣿⣿⣾⣦⡙⠻⣷⣾⣿⠃⠿⠋⠁⠄⠄⠄⠄⠄⢀⣠⣴</a><br />')
+  .typeString('<a href="ascii.html" target="_blank">⣿⣿⣿⣶⣶⣮⣥⣒⠲⢮⣝⡿⣿⣿⡆⣿⡿⠃⠄⠄⠄⠄⠄⠄⠄⣠⣴⣿⣿⣿</a><br />')
+  .typeString('<br /><br />')
+  .typeString('Hello there, I am an experienced LSL scripter and available for hire.')
+  .typeString('<br /> <br />I have worked on a variety of inworld projects over the years. While coding is my primary focus, I have a deep understanding of game asset creation in general. <br /> <br />')
+  .pauseFor(100)
   .typeString('If you need assistance with your project, just contact me inworld!')
-  .typeString('<br /> <br />Live long and prosper!')
+  .typeString('<br /> <br /> <a href="vulcan_salute.html" target="_blank">Live long and prosper!</a> <br />')
   .typeString('<br /> <br />Find me online: <br /> <br />')
-  .pauseFor(300)
-  .typeString('<a href="https://world.secondlife.com/resident/140daf33-db90-4451-954c-4fbeceb186c8">Second Life</a> <br />')
-  .typeString('<a href="https://github.com/natc0d3s/">GitHub</a> <br />')
+  .pauseFor(100)
+  .typeString('<a href="https://world.secondlife.com/resident/140daf33-db90-4451-954c-4fbeceb186c8" target="_blank"">Second Life</a> <br />')
+  .typeString('<a href="https://github.com/natc0d3s/" target="_blank">GitHub</a> <br />')
     
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
- 
-  
-
